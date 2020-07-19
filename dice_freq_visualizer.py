4f2215aadec2bce8ff72630fad5492f422207fab
jabberwoockey <jabberwoockey@gmail.com>
2020-07-19 19:24:03 +0300

    Initial commit

---
+++ dice_freq_visualizer.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+# In order to run the script plotly package should be installed:
+# pip3 install plotly
+
+from random import randint
+from sys import argv, exit
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
+# Create a die class
+class Die:
+    """A class representing a single die."""
+    def __init__(self, num_sides=6):
+        """Assume a six-sided die."""
+        self.num_sides = num_sides
+    def __str__(self):
+        return f'A {self.num_sides}-sided die.'
+    def roll(self):
+        """"Return a random value between 1 and number of sides."""
+        return randint(1, self.num_sides)
+
+# Initial variables
+times = 1000
+
+# Create dice
+if len(argv) == 1:
+    script = argv
+    print(f'Usage: {script[0]} [<die> [<die>] ...] [number of times]\n' +
+        f' '*7+f'{script[0]} 6 (for d6 1000 times)\n' +
+        f' '*7+f'{script[0]} 6 1000 (for d6 and d10 10000 times)\n' +
+        f' '*7+f'{script[0]} 6 10 10000 (for d6 and d10 10000 times)\n' +
+        f' '*7+f'{script[0]} 100 100 100 100000 (for three d100 100000 times)')
+    exit(1)
+elif len(argv) == 2:
+    script, sides = argv
+    dice = [Die(int(sides))]
+else:
+# len(argv) > 2:
+    script, *sides_list, times = argv
+    dice = [Die(int(sides)) for sides in sides_list]
+
+dice_sting = ["d" + str(die.num_sides) for die in dice]
+
+# Make some rolls, and store results in a list
+results = [sum([die.roll() for die in dice]) for i in range(int(times))]
+
+# Analyze the results
+max_result = sum([die.num_sides for die in dice])
+min_result = len(dice)
+frequencies = [results.count(i) for i in range(min_result, max_result+1)]
+
+# Create a .csv and print results on the screen
+filename = f'{"_".join(dice_sting)}_{times}times'
+with open(f'{filename}.csv', 'w') as f:
+    f.write('number,frequency\n')
+    for i in range(min_result, max_result+1):
+        print(f'{i:>3}: {frequencies[i - min_result]:>7}')
+        f.write(f'{i},{frequencies[i - min_result]}\n')
+print('-' * 12 + '\n' + f'   {sum(frequencies):>9}')
+
+# Visualize the results
+x_values = list(range(min_result, max_result+1))
+data = [Bar(x=x_values, y=frequencies)]
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency of Result'}
+my_layout = Layout(title=f'Results of rolling the dice ' +
+                         f'{", ".join(dice_sting)} ' +
+                         f'{times} times',
+                   xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout},
+             filename=f'{filename}.html')
---
+++ word_frequency.py
@@ -0,0 +1,72 @@
+from sys import argv, exit
+import os
+import re
+
+pattern = re.compile("^(?:[-_\\\(\[\'\"«]+)?([\w]([\w]*)?([-\'][\w]+)?)",
+          re.IGNORECASE)
+
+# Try to open a file
+try:
+    script, filename = argv
+    with open(filename) as file:
+        content = file.read()
+except ValueError:
+    print('Usage: word_frequency.py file.txt')
+    exit(1)
+except FileNotFoundError:
+    print(f'No such file or directory: {filename}')
+    exit(1)
+
+# Initial variables
+base = os.path.basename(filename).split(".")[0]
+words = content.split()
+words_capt = []
+words_noncapt = []
+words_capt_dict = {}
+count = 0
+
+# Create a words list
+for word in words:
+    re_word = pattern.search(word)
+    if re_word != None and not re_word.group(1).isdigit():
+        words_capt.append((re_word.group(1).lower()))
+    else:
+        words_noncapt.append(word.lower())
+
+# Remove 's endings
+for i in range(len(words_capt)):
+    if words_capt[i][-2:] == "'s":
+        words_capt[i] = words_capt[i][:-2]
+
+# Create a ditionary with words
+for word in words_capt:
+    if word not in words_capt_dict:
+        words_capt_dict[word] = 1
+    else:
+        words_capt_dict[word] += 1
+
+# Sort the dictionary
+result = {k: v for k, v in sorted(words_capt_dict.items(),
+          key=lambda item: item[1], reverse=True)}
+
+# Write output files
+try:
+    with open(f'{base}_words.csv', 'w') as res_words:
+        for k, v in result.items():
+            print(f'{k:>30}:' + f' {v:>7}')
+            res_words.write(f'{k};{v}\n')
+            count += v
+    with open(f'{base}_nonwords.csv', 'w') as res_nonwords:
+        for word in set(words_noncapt):
+            res_nonwords.write(word + '\n')
+except PermissionError:
+    print("Close related .csv files!")
+    exit(1)
+
+# Print footer
+print(f'{count:>39}\n' + '-'*39)
+print(f'Total words                   : {len(words):>7}')
+print(f'Captured words                : {len(words_capt):>7}')
+print(f'Non-captured words            : {len(words_noncapt):>7}')
+print(f'Distinct words                : {len(set(words)):>7}')
+print(f'Distinct captured words       : {len(set(words_capt)):>7}')
